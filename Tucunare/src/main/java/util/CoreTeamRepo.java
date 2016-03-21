d407042afa50dd38631a2bf4fe6d04904a1513e8
mlimeira <mlimeira@ic.uff.br>
2016-03-21 12:57:29 -0300

    Classes para extrair a rede social.

--- Tucunare/src/main/java/util/CoreTeamRepo.java
+++ Tucunare/src/main/java/util/CoreTeamRepo.java
@@ -14,9 +14,9 @@ import control.Commits;
 //para executar no GHTorrent basta digitar o nome do aqruivo gerado no diretório 
 public class CoreTeamRepo {
 	public static void main(String[] args) throws UnknownHostException {
-		ArrayList<String> list = Commits.getCoreTeamList3("js_of_ocaml","ocsigen");
+		ArrayList<String> list = Commits.getCoreTeamList3("frappe","frappe");//repo, owner
 		//Collections.sort(list);
-		File file = new File("c:/Ruby200/lib/ruby/gems/2.0.0/gems/ghtorrent-0.10/projects/100-200/","CoreTeam4.sh");
+		File file = new File("c:/Ruby200/lib/ruby/gems/2.0.0/gems/ghtorrent-0.10/projects/100-200/","CoreTeam1.4.sh");
 		try{
 			FileWriter fw = new FileWriter(file, true);
 			fw.write("#!/bin/bash"+"\n");
--- Tucunare/src/main/java/util/PullProject.java
+++ Tucunare/src/main/java/util/PullProject.java
@@ -1,11 +1,14 @@
 package util;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 
 public class PullProject {
 
@@ -16,15 +19,16 @@ public class PullProject {
 		fields.put("number", 1);
 		fields.put("_id", 0);
 		fields.put("repo", 1);
+		fields.put("owner", 1);
 		BasicDBObject query = new BasicDBObject("state", "closed"); //consulta com query
-		
 		Object [] repos = dbcPullRequest.distinct("repo", query).toArray();
 		for (Object object : repos) {
 			query.append("repo", object.toString());
-			System.out.println(object.toString()+","+dbcPullRequest.find(query,fields).count());
+			DBCollection dbcRepo = db.getCollection("pull_requests");
+			String repo = object.toString();
+			BasicDBObject queryRepo = new BasicDBObject("repo", repo);
+			String owner = (String) dbcRepo.findOne(queryRepo).get("owner");
+			System.out.println(owner+"/"+object.toString()+","+dbcPullRequest.find(query,fields).count());
 		}
-		 
-
 	}
-
 }
